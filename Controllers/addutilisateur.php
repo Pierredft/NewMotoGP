03176f8fa40d37dee4c099b54f838bd372b56f4b
pierre defauquet <pierredefauquet@gmail.com>
2023-11-23 14:31:40 +0100

    😫

--- Controllers/addutilisateur.php
+++ Controllers/addutilisateur.php
@@ -1,4 +1,4 @@
-<?php
+<?php if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 require_once "../includes/connexiondb.php";
 //  ajout des entrée à la base de données
 $con = connectdb();
@@ -16,35 +16,55 @@ if (isset($_POST['Nom_User']) && isset($_POST['Prenom_User']) && isset($_POST['M
     $confpass   =   $_POST['confpass'];
     $regpass    =   "/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#\+\-\^\[\]])[A-Za-z\d#\+\-\^\[\]]{8,}$/";
     $hash       =   password_hash($pass, PASSWORD_BCRYPT);
-    $vmail      =   $con ->prepare("SELECT * FROM UTILISATEUR WHERE Mail_User = ?");
-    $vmail -> execute([$mail]);
-    $usermail = $vmail->fetch();
+    $vmail      =   $con ->prepare("SELECT * FROM UTILISATEUR WHERE Mail_User=?");
+    $vmail      ->  execute([$mail]);
+    $usermail   =   $vmail->fetch();
+    
 
     $uploaddir = '../img/profil/';
     $uploadfile = $uploaddir . basename($_FILES['Img_Profile']['name']);
     move_uploaded_file($_FILES['Img_Profile']['tmp_name'],$uploadfile);
 
-    // if ($usermail) {
-    //     echo"l'e-mail éxiste déjà";
-    // }else 
-    if (!preg_match ($nomreg, $nom)) {
-        echo'Le nom doit contenir minimum 3 caractère et pas de chiffre';
-    } else if (!preg_match ($prenomreg, $prenom)) {
-        echo'Le prenom doit contenir minimum 3 caractère et pas de chiffre';
-    }else if (!preg_match ($regpass , $pass)) {
-        echo'Le mot de passe ne remplit pas les conditions';
+
+    if (!preg_match ($nomreg, $nom)|| $nom =="") {
+        $_SESSION['erreur1']=1;
+        header('Refresh:0; url=../views/register.php');
+    } else if (!preg_match ($prenomreg, $prenom)|| $prenom =="") {
+        $_SESSION["erreur2"]= 1;
+        header("Refresh:0; url=../views/register.php");
+    }else if (!preg_match ($regmail, $mail) || $mail =="") {
+        $_SESSION["erreur3"]= 1;
+        header("Refresh:0; url=../views/register.php");
+    }else if ($usermail) {
+        $_SESSION["erreur4"]= 1;
+        header("Refresh:0; url=../views/register.php");
+    }else if (!preg_match ($regpass , $pass) || $prenom =="") {
+        $_SESSION["erreur5"]= 1;
+        header("Refresh:0; url=../views/register.php");
     }else if ($pass != $confpass) {
-        echo'Les mots de passe ne sont pas identique';
-    } else 
+        $_SESSION["erreur6"]= 1;
+        header("Refresh:0; url=../views/register.php");
+    } else
+    
+    
     {
     //on fait notre requête sql avec le prépare
     $req = $con->prepare('INSERT INTO UTILISATEUR (Nom_User, Prenom_User, Mail_User, MDP_User, Img_Profile) VALUES (?,?,?,?,?)');
     //puis on exécute notre requête
+
     $req->execute(array($nom, $prenom, $mail, $hash, $uploadfile));
+    $_SESSION['erreur1'] = 0;
+    $_SESSION['erreur2'] = 0;
+    $_SESSION['erreur3'] = 0;
+    $_SESSION['erreur4'] = 0;
+    $_SESSION['erreur5'] = 0;
+    $_SESSION['erreur6'] = 0;
+    $_SESSION['success'] = 1;
     // retour sur le tableau des auteurs
     header('location: ../views/connexion.php');
     echo'vous etes con';
     exit;
-}}
+    }
+}
 
 ?>
--- Views/register.php
+++ Views/register.php
@@ -1,4 +1,5 @@
-<?php   require_once "../includes/head.php";
+<?php   if (session_status() !== PHP_SESSION_ACTIVE) session_start();
+        require_once "../includes/head.php";
         require_once "../controllers/utilisateurController.php";
         require_once "../controllers/addutilisateur.php";
 ?>
@@ -13,16 +14,32 @@
     <main>
         <div class="container">
             <form id="signup" class="form" action="../controllers/addutilisateur.php" method="post" enctype="multipart/form-data">
+                <?php if (isset ($_SESSION['succes']) && $_SESSION['succes'] == 1) {
+                    echo '<p class="sucess">Tu est déja inscrit</p>';
+                }
+                ?>
                 <div class="flex">
                     <div class="form-field error success size">
                         <label for="Nom_User"><span class="color texteMotoGP">Nom:</span></label>
                         <input type="text" name="Nom_User" id="username" autocomplete="off">
+                        <span id="erreur1" name="erreurNom"></span>
+                        <?php 
+                        if (isset ($_SESSION['erreur1']) && $_SESSION['erreur1']==1) {
+                            echo'<p class="erreur">Nom incorrect ou trop court (3 caractères minimum et sans chiffres) !</p>';
+                        }
+                        ?>
                         <small></small>
                     </div>
 
                     <div class="form-field error success size">
                         <label for="Prenom_User"><span class="color texteMotoGP">Prénom:</span></label>
                         <input type="text" name="Prenom_User" id="firstname" autocomplete="off">
+                        <span id="erreur2" name="erreurPrenom"></span>
+                        <?php 
+                        if (isset ($_SESSION['erreur2']) && $_SESSION['erreur2']==1) {
+                            echo'<p class="erreur">Prenom incorrect ou trop court (3 caractères minimum et sans chiffres)!</p>';
+                        }
+                        ?>
                         <small></small>
                     </div>
                 </div>
@@ -30,29 +47,49 @@
 
                 <div class="form-field error success">
                     <label for="Mail_User"><span class="color texteMotoGP">E-mail:</span></label>
-                    <input type="text" name="Mail_User" id="email" autocomplete="off">
+                    <input type="email" name="Mail_User" id="email" autocomplete="off">
+                    <span id="erreur3" name="erreurMail"></span>
+                        <?php 
+                        if (isset ($_SESSION['erreur3']) && $_SESSION['erreur3']==1) {
+                            echo'<p class="erreur">Format du mail incorrect!</p>';
+                        } else if (isset ($_SESSION['erreur4']) && $_SESSION['erreur4']==1) {
+                            echo'<p class="erreur">email existe deja!</p>';
+                        }
+                        ?>
                     <small></small>
                 </div>
 
                 <div class="form-field error success">
                     <label for="MDP_User"><span class="color texteMotoGP">Mot de passe:</span></label>
                     <input type="password" name="MDP_User" id="password" autocomplete="off">
+                    <span id="erreur5" name="erreurPassword"></span>
+                        <?php 
+                        if (isset ($_SESSION['erreur5']) && $_SESSION['erreur5']==1) {
+                            echo'<p class="erreur">Format du mot de passe incorrect (doit contenir 8 caractère minimum, une majuscule, un chiffre et un caractère spéciale)!</p>';
+                        }
+                        ?>
                     <small></small>
                 </div>
 
                 <div class="form-field error success">
                     <label for="confpass"><span class="color texteMotoGP">Confirmation du mot de passe:</span></label>
                     <input type="password" name="confpass" id="confirm-password" autocomplete="off">
+                    <span id="erreur6" name="erreurConfPassword"></span>
+                        <?php 
+                        if (isset ($_SESSION['erreur6']) && $_SESSION['erreur6']==1) {
+                            echo'<p class="erreur">Le mot de passe est différent de celui du dessus !</p>';
+                        }
+                        ?>
                     <small></small>
                 </div>
 
                 <div class="form-field error success">
                     <label for="Img_Profile"><span class="color texteMotoGP">Selectionner votre photo de profile</span></label>
-                    <input type="file" name="Img_Profile" id="fichier">
+                    <input type="file" name="Img_Profile" id="fichier" required>
                     <small></small>
                 </div>
 
-                <input type="checkbox" name="rgpd" value="rgpd"><span>En cochant cette case vous accepter l'utilisation de vos donées</span>
+                <input type="checkbox" name="rgpd" value="rgpd" required><span>En cochant cette case vous accepter l'utilisation de vos donées</span>
 
                 <div class="test">
                     <input type="submit" value="S'inscrire" class="btn motoGP">
